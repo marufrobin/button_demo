53ed5d55608bd4df768375403a0d92abb357a68b
Maruf Robin <maruf.robin@northsouth.edu>
2022-10-07 01:00:38 +0600

    Appbar done..

--- lib/button_demo.dart
+++ lib/button_demo.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -11,146 +9,152 @@ class ButtonDemo extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-            backgroundColor: Color(0xffE5D2D2),
-            body: Padding(
-              padding: const EdgeInsets.only(top: 10, right: 25, left: 25),
-              child: Column(
+      backgroundColor: Color(0xffE5D2D2),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 10, right: 25, left: 25),
+        child: Column(
+          children: [
+            //AppBar design here
+            customAppBar(),
+            SizedBox(
+              height: 28,
+            ),
+            //--------------------------------
+            // popular japan moscow london
+            Container(
+              width: 376,
+              height: 44,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Color(0xff504949),
+              ),
+              child: Row(
                 children: [
-                  //AppBar design here
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Icon(
-                        Icons.menu,
-                        size: 20,
-                      ),
-                      // SizedBox(
-                      //   width: 50,
-                      // ),
-                      Container(
-                        height: 50,
-                        width: 130,
-                        child: Stack(
-                          children: [
-                            Container(
-                              height: 25,
-                              width: 25,
-                              color: Color(0xffEA4545),
-                            ),
-                            Positioned(
-                              bottom: 8,
-                              left: 12,
-                              child: Text(
-                                "Discover",
-                                style: TextStyle(
-                                    fontSize: 30, fontFamily: "Roboto"),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      Icon(
-                        Icons.search,
-                        size: 20,
-                      )
-                    ],
-                  ),
                   SizedBox(
-                    height: 28,
+                    width: 18,
                   ),
-
-                  //--------------------------------
-                  // popular japan moscow london
-                  Container(
-                    width: 376,
-                    height: 44,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      color: Color(0xff504949),
-                    ),
-                    child: Row(
-                      children: [
-                        SizedBox(
-                          width: 18,
-                        ),
-                        Text(
-                          "Popular",
-                          style: TextStyle(
-                            fontFamily: "Roboto",
-                            fontSize: 18,
-                            color: Colors.white,
-                          ),
-                        ),
-                        SizedBox(
-                          width: 18,
-                        ),
-                        Text("Japan",
-                            style: TextStyle(
-                              fontFamily: "Roboto",
-                              fontSize: 18,
-                              color: Colors.white,
-                            )),
-                        SizedBox(
-                          width: 18,
-                        ),
-                        Text("Moscow",
-                            style: TextStyle(
-                              fontFamily: "Roboto",
-                              fontSize: 18,
-                              color: Colors.white,
-                            )),
-                        SizedBox(
-                          width: 18,
-                        ),
-                        Text("London",
-                            style: TextStyle(
-                              fontFamily: "Roboto",
-                              fontSize: 18,
-                              color: Colors.white,
-                            )),
-                        SizedBox(
-                          width: 18,
-                        ),
-                      ],
+                  Text(
+                    "Popular",
+                    style: TextStyle(
+                      fontFamily: "Roboto",
+                      fontSize: 18,
+                      color: Colors.white,
                     ),
                   ),
                   SizedBox(
-                    height: 28,
+                    width: 18,
                   ),
-                  Stack(
-                    children: [
-                      Container(
-                        width: 370,
-                        height: 285,
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(30),
-                            image: DecorationImage(
-                                image: AssetImage("images/image1.jpg"),
-                                fit: BoxFit.cover)),
-                      ),
-                      Container(
-                        width: 200,
-                        height: 60,
-                        decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20)),
-                        child: Text(
-                          "Discover more",
-                          style: TextStyle(
-                            fontFamily: "Pacifico",
-                            fontSize: 18,
-                            color: Colors.red,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      )
-                    ],
-                  )
+                  Text("Japan",
+                      style: TextStyle(
+                        fontFamily: "Roboto",
+                        fontSize: 18,
+                        color: Colors.white,
+                      )),
+                  SizedBox(
+                    width: 18,
+                  ),
+                  Text("Moscow",
+                      style: TextStyle(
+                        fontFamily: "Roboto",
+                        fontSize: 18,
+                        color: Colors.white,
+                      )),
+                  SizedBox(
+                    width: 18,
+                  ),
+                  Text("London",
+                      style: TextStyle(
+                        // decoration: TextDecoration.lineThrough,
+                        fontFamily: "Roboto",
+                        fontSize: 18,
+                        color: Colors.white,
+                      )),
+                  SizedBox(
+                    width: 18,
+                  ),
                 ],
               ),
+            ),
+            SizedBox(
+              height: 28,
+            ),
+            Stack(
+              children: [
+                //image position
+                Container(
+                  width: 370,
+                  height: 285,
+                  decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(30),
+                      image: DecorationImage(
+                          image: AssetImage("images/image1.jpg"),
+                          fit: BoxFit.cover)),
+                ),
+
+                //discover more
+                Positioned(
+                  : 50,
+                  child: Container(
+                    width: 200,
+                    height: 60,
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Text(
+                      "Discover more",
+                      style: TextStyle(
+                        // decoration: TextDecoration.lineThrough,
+                        fontFamily: "Pacifico",
+                        fontSize: 18,
+                        color: Colors.red,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              ],
             )
-            // body: Container(child: ),
-            ));
+          ],
+        ),
+      ),
+    ));
+  }
+
+  Row customAppBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Icon(
+          Icons.menu,
+          size: 20,
+        ),
+        Container(
+          height: 50,
+          width: 130,
+          child: Stack(
+            children: [
+              Container(
+                height: 25,
+                width: 25,
+                color: Color(0xffEA4545),
+              ),
+              Positioned(
+                bottom: 8,
+                left: 12,
+                child: Text(
+                  "Discover",
+                  style: TextStyle(fontSize: 30, fontFamily: "Roboto"),
+                ),
+              )
+            ],
+          ),
+        ),
+        Icon(
+          Icons.search,
+          size: 20,
+        )
+      ],
+    );
   }
 }
